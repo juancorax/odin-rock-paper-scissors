e42740d3db023df19102ccc423088b6cbe3a2d56
Juan Manuel Corigliano Giuliani <126042007+juancorax@users.noreply.github.com>
2024-03-18 12:19:35 -0300

    feat: add a function that plays a single round

--- script.js
+++ script.js
@@ -17,3 +17,23 @@ function getComputerChoice() {
     return "scissors";
   }
 }
+
+function playRound(playerSelection, computerSelection) {
+  let playerSelection = playerSelection.toLowerCase();
+
+  if (playerSelection === computerSelection) {
+    return "Tie!";
+  } else if (playerSelection === "rock" && computerSelection === "scissors") {
+    return "You Win! Rock beats Scissors";
+  } else if (playerSelection === "rock" && computerSelection === "paper") {
+    return "You Lose! Paper beats Rock";
+  } else if (playerSelection === "paper" && computerSelection === "rock") {
+    return "You Win! Paper beats Rock";
+  } else if (playerSelection === "paper" && computerSelection === "scissors") {
+    return "You Lose! Scissors beats Paper";
+  } else if (playerSelection === "scissors" && computerSelection === "paper") {
+    return "You Win! Scissors beats Paper";
+  } else if (playerSelection === "scissors" && computerSelection === "rock") {
+    return "You Lose! Rock beats Scissors";
+  }
+}
